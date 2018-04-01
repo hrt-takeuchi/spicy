d9b40cd94d18c45dc620fd99b69ca879a12eb009
takeuchi <test>
2018-04-01 16:08:14 +0900

    first commit

---
+++ aiwolfpy/spicy/__init__.py
@@ -0,0 +1,6 @@
+from __future__ import print_function, division 
+from .predictor_cedec2017 import Predictor_15
+from .predictor_cedec2017_5 import Predictor_5
+from .tensor5460 import Tensor5460
+from .tensor60 import Tensor60
+
