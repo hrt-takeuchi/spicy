c0996ca1cf5dc6da5997a70ac6cbcd78d7536a30
takeuchi <test>
2018-05-19 14:38:55 +0900

    微調整

--- spicy.py
+++ spicy.py
@@ -396,6 +396,6 @@ if __name__ == '__main__':
     aiwolfpy.connect_parse(agent)
     t2 = time.time()
     # 経過時間を表示
-    elapsed_time = t2-t1    
+    elapsed_time = str('{:.2f}'.format(t2-t1))
     print(f"経過時間：{elapsed_time}")
     read_record()
